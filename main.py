a64acfb6fe9ce7b036657103a9b4eba5cff63812
mzivic7 <marko.b.zivic@gmail.com>
2023-01-01 18:36:38 +0100

    Typo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ try:
     with open("config.conf") as file:   # open settings file
         conf = file.readlines()   # read settings
         txt_encoding = conf[0].replace("\n", "")   # read first line and remove newline
-        bellow = eval(conf[1].replace("\n", ""))   # read second line and remove newline
+        below = eval(conf[1].replace("\n", ""))   # read second line and remove newline
         filetypes = conf[2].replace("\n", "")   # read third line and remove newline
         filetypes = filetypes.split(", ")   # convert string to list
         keywords = conf[3].replace("\n", "")   # read fourth line and remove newline
@@ -38,7 +38,7 @@ for filename in file_list:   # for each subtitle file in dir
             lines = file.readlines()   # get list of lines
             with open(filename, 'w', encoding=txt_encoding) as file:   # open file
                 for line in lines:   # for each line
-                    if bellow is True:
+                    if below is True:
                         if mem is True:   # if above line is deleted:
                             if line != "\n" and line != " \n":   # if current line is not empty or whitespace:
                                 line = ""   # delete all text   
